e3fec1ad0c1144b0a74b9c20eda124d251b7c559
Higor Allan <higor.allan21@gmail.com>
2022-08-21 14:07:09 -0300

    finalizando

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -70,7 +70,7 @@ export function Subscribe(){
           </form>
         </div>
       </div>
-      <img src="/src/assets/code-mockup.png" alt="" className="mt-10" />
+      <img src="./src/assets/code-mockup.png" alt="" className="mt-10" />
     </div>
   )
 }
