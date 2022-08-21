aeda847b1252cb4d0dc350bf4206c9e527e82cf3
Higor Allan <higor.allan21@gmail.com>
2022-08-21 14:33:29 -0300

    tentando resolver o problema do link nao entrar

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -4,17 +4,17 @@ import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event(){
-  // const { slug } = useParams<{slug: string}>();
+  const { slug } = useParams<{slug: string}>();
 
   return(
     <div className="flex flex-col min-h-screen">
       <Header />
 
       <main className="flex flex-1">
-        {/* { slug 
-          ? <Video lessonSlug={slug} /> */}
+        { slug 
+          ? <Video lessonSlug={slug} />
           : <div className="flex-1"></div>
-        {/* } */}
+        }
         <Sidebar />
       </main>
     </div>
